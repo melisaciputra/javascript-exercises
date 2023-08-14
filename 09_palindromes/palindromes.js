10da2dfbc6141768357b332b79d6ba6f6b7d90f3
melisaciputra <melisa.ciputra@gmail.com>
2023-08-14 19:27:20 +0800

    Modified exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const arr = Array.from(str.toUpperCase().replace(/(?![0-9A-Z])./g, ''));
+    let i = 0;
+    let j = arr.length-1;
+    while(i <= j){
+        if (arr[i] != arr[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
