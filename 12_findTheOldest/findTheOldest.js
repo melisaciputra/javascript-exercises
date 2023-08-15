50939daba00f5d8bf353a6bfdad809c0c8a0f924
melisaciputra <melisa.ciputra@gmail.com>
2023-08-15 15:00:56 +0800

    Completed exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let result = arr.reduce((oldest, current) => {
+        if (!("yearOfDeath" in oldest)) oldest.yearOfDeath = new Date().getFullYear();
+        if (!("yearOfDeath" in current)) current.yearOfDeath = new Date().getFullYear();
 
+        if((oldest.yearOfDeath-oldest.yearOfBirth) < (current.yearOfDeath-current.yearOfBirth)) {
+            oldest = current;
+        }
+        console.table(oldest);
+        return oldest;
+    },arr[0]);
+    return result;
 };
 
 // Do not edit below this line
